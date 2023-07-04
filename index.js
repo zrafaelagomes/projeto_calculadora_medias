91c2d51f6bc3905feff0fde3683bd96a99a45773
Rafaela Gomes <rafagiza@hotmail.com>
2023-07-04 06:47:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
 const atividades = [];
 const notas = [];
-const spanAprovado = <span class="resultado aprovado">Aprovado</span>;
-const spanReprovado = <span class="resultado reprovado">Reprovado</span>;
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 const notaMinima = parseFloat(prompt("Digite a nota mínima"));
 
 let linhas = '';
