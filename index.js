d6fbda7b8334a9b5e6befab4539e3890e0f26b32
Rafaela Gomes <rafagiza@hotmail.com>
2025-04-01 19:21:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+const imgAprovado = '<img src="./images/aprovado.png" alt = "emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt = "emoji decepcionado" />';
 const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
@@ -23,17 +23,17 @@ function adicionaLinha() {
 
     if (atividades.includes(inputNomeAtividade.value)) {
         alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
-    } else {
+    } else { 
         atividades.push(inputNomeAtividade.value);
         notas.push(parseFloat(inputNotaAtividade.value));
-
+    
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado }</td>`;
-        linha += '</tr>';
-
-        linhas += linha;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += `</tr>`;
+    
+        linhas += linha;   
     }
 
     inputNomeAtividade.value = '';
@@ -48,8 +48,8 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML.HTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML.HTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
